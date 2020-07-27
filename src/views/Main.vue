eec7274c1189044475cdd8c88671d0f3465662ab
Daniel Kiss <kissd621@gmail.com>
2020-07-27 06:58:50 +0200

    fixed background disappearing when scrolling

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="main">
-    <!-- <transition name="component-fast-fade" mode="out-in"> -->
     <router-view></router-view>
-    <!-- </transition> -->
   </div>
 </template>
 
@@ -13,31 +11,22 @@ export default {
 </script>
 
 <style scoped>
-/* TRANSITION */
-/* .component-fast-fade-enter-active,
-.component-fast-fade-leave-active {
-  transition: all 1s ease-out;
-}
-.component-fast-fade-enter,
-.component-fast-fade-leave-to {
-  
-} */
-
-div.container-md::before {
+#main::before {
   content: '';
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background: url('~@/assets/background.jpg') no-repeat center center fixed;
+  background: url('~@/assets/background.jpg') repeat center center fixed;
   background-size: cover;
   z-index: -2;
+  overflow: hidden;
 }
-div.container-md::after {
+#main::after {
   content: '';
   background-color: rgb(0, 0, 0, 0.7);
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
