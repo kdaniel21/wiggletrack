89bf1d5d5ba47f302ea779a42d18237a1131ebc8
Daniel Kiss <kissd621@gmail.com>
2020-08-09 11:45:13 +0200

    created multi line card decks

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -13,9 +13,7 @@
 
         <slot name="details" :product="product"></slot>
         <router-link :to="`/products/${product._id}`">
-          <b-button pill variant="primary" class="float-right mt-1"
-            >View Product</b-button
-          >
+          <b-button pill variant="primary" class="float-right mt-1">View Product</b-button>
         </router-link>
       </b-card>
     </b-card-group>
@@ -31,11 +29,17 @@ export default {
 
 <style scoped>
 .card {
+  min-width: 230px;
   max-width: 250px;
+  margin-bottom: 1rem;
 }
 .img {
   max-height: 248px;
   max-width: 248px;
+}
+.card-deck {
+  max-width: 100%;
+  flex-flow: wrap;
 }
 b-card-subtitle {
   line-height: 24px;
