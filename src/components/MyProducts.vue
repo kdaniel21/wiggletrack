c5546a1fe884f11f8cd876cfc0138cfc74bd7cec
Daniel Kiss <kissd621@gmail.com>
2020-08-10 09:19:46 +0200

    implemented pagination for own products

--- src/components/MyProducts.vue
+++ src/components/MyProducts.vue
@@ -2,20 +2,36 @@
   <div id="my-products">
     <product-list :products="products">
       <template v-slot:img-btn="slotData">
-        <b-button class="delete-btn" @click="showConfirmationModal(slotData.product._id)">
+        <b-button
+          class="delete-btn"
+          @click="showConfirmationModal(slotData.product._id)"
+        >
           <i class="fas fa-trash"></i>
         </b-button>
       </template>
 
       <template v-slot:details="slotData">
-        <b-badge pill :variant="slotData.product.notifications ? 'primary' : 'danger'">
+        <b-badge
+          pill
+          :variant="slotData.product.notifications ? 'primary' : 'danger'"
+        >
           Notifications
           {{ slotData.product.notifications ? 'enabled' : 'disabled' }}
         </b-badge>
       </template>
     </product-list>
+    <!-- PAGINATION -->
+    <Pagination
+      :numOfResults="numOfBookmarkedProducts"
+      :resultsPerPage="resultsPerPage"
+      @page-change="getUserProducts"
+    />
     <!-- CONFIRMATION MODAL -->
-    <b-modal id="confirmation-modal" title="Are you sure?" @ok="unbookmarkProduct()">
+    <b-modal
+      id="confirmation-modal"
+      title="Are you sure?"
+      @ok="unbookmarkProduct()"
+    >
       <p>
         Are you sure that you want to unbookmark this product? This will disable
         any notifications.
@@ -28,22 +44,47 @@
 import axios from 'axios';
 import toaster from '../mixins/toaster';
 import ProductList from './ProductList';
+import Pagination from '../components/Pagination';
 
 export default {
   name: 'MyProducts',
   mixins: [toaster],
   components: {
-    ProductList
+    ProductList,
+    Pagination
   },
   data() {
     return {
       selectedProduct: null,
-      products: []
+      products: [],
+      resultsPerPage: 3
     };
   },
+  computed: {
+    numOfBookmarkedProducts() {
+      return this.$store.state.user.products.length;
+    }
+  },
   methods: {
-    getUserProducts() {
-      return axios.get('/users/me/products').then(res => res.data.data);
+    // Request own products from the server
+    getUserProducts(page = 1) {
+      const params = {
+        page,
+        limit: this.resultsPerPage
+      };
+
+      return axios
+        .get(`/users/me/products`, { params })
+        .then(res => this.setProducts(res.data.data));
+    },
+    // Parse and set products
+    setProducts(products) {
+      this.products = products.map(product => {
+        return {
+          ...product.product,
+          notifications: product.notifications
+        };
+      });
     },
     showConfirmationModal(id) {
       this.selectedProduct = id;
@@ -71,16 +112,9 @@ export default {
       );
     }
   },
+  // Fetch first page
   async created() {
-    const products = await this.getUserProducts();
-
-    // Transform response object
-    this.products = products.map(product => {
-      return {
-        ...product.product,
-        notifications: product.notifications
-      };
-    });
+    await this.getUserProducts();
   }
 };
 </script>
